1f8d04f3a410f64cf7ff105e058676b85da50fd1
Ernest <muiboonyang@gmail.com>
2022-03-22 22:29:10 +0800

    Added tests for backend

--- tests/server.test.js
+++ tests/server.test.js
@@ -3,7 +3,7 @@ const server = require("../server.js");
 const supertest = require("supertest");
 const requestWithSupertest = supertest(server);
 
-describe("Short Urls", () => {
+describe("Seed Users", () => {
   it("POST /seeduser should seed users", async () => {
     const res = await requestWithSupertest.post("/seeduser");
     expect(res.status).toEqual(200);
@@ -11,17 +11,38 @@ describe("Short Urls", () => {
   });
 });
 
-// describe("Short Urls", () => {
-//   it("GET /shortUrl should show all shortUrl", async () => {
-//     const res = await requestWithSupertest.get("/nIHDdCael");
-//     expect(res.status).toEqual(200);
-//     expect(res.type).toEqual(expect.stringContaining("json"));
-//     expect(res.body).toHaveProperty("nIHDdCael");
-//   });
-// });
+describe("Short Urls - Redirect", () => {
+  it("GET /:shortUrl should redirect users to long url", async () => {
+    const res = await requestWithSupertest.get("/nIHDdCael");
+    expect(res.status).toEqual(302);
+    expect(res.redirect);
+  });
+});
 
-// it("GET /users/:id should show a user", async () => {
-//   const res = await requestWithSupertest.get("/users/3");
-//   expect(res.statusCode).toEqual(200);
-//   expect(res.body).toHaveProperty("user3");
-// });
+describe("Short Urls - Edit Long Url", () => {
+  it("POST /:shortUrl/update should update long url", async () => {
+    const res = await requestWithSupertest.post("/nIHDdCael/update");
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining("json"));
+  });
+});
+
+describe("Delete - All Users, All Urls, One Url", () => {
+  it("POST /delete/allurl should delete all short and long urls", async () => {
+    const res = await requestWithSupertest.post("/delete/allurl");
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining("json"));
+  });
+
+  it("POST /delete/alluser should delete all users", async () => {
+    const res = await requestWithSupertest.post("/delete/alluser");
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining("json"));
+  });
+
+  it("POST /delete/nIHDdCael should delete its short and long urls", async () => {
+    const res = await requestWithSupertest.post("/delete/nIHDdCael");
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining("json"));
+  });
+});
