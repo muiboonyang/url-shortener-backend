285d6aec5232e5753ccd96db3d0c5a56841fdd58
Ernest <muiboonyang@gmail.com>
2022-04-08 10:18:00 +0800

    Update urls.js

--- models/urls.js
+++ models/urls.js
@@ -1,6 +1,19 @@
 const mongoose = require("mongoose");
 const shortId = require("shortid");
 
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+console.log(makeid(5));
+
 const UrlSchema = new mongoose.Schema(
   {
     username: { type: String, required: true },
@@ -12,7 +25,8 @@ const UrlSchema = new mongoose.Schema(
     short: {
       type: String,
       required: true,
-      default: shortId.generate,
+      // default: shortId.generate,
+      default: makeid(5),
     },
 
     clicks: {
